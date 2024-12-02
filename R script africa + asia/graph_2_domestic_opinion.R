72db668bdb05c87ce525f3db68a9567522709790
bunmam-ctrl <catherinatran.12@gmail.com>
2024-12-02 17:17:07 -0600

    Update on graph 2 africa

--- R script africa + asia/graph_2_domestic_opinion.R
+++ R script africa + asia/graph_2_domestic_opinion.R
@@ -4,26 +4,23 @@
 violence_tidy|>
   group_by( justify_violence)|>
   drop_na()|>
-  ggplot(aes(x = justify_violence, y = value_agree, fill = subregion))+
-  geom_col()+
+  ggplot(aes(x = justify_violence, y = value_agree))+
+  geom_col(color = "aquamarine4")+
   theme_minimal() +
-  scale_fill_brewer(palette = "Set2")+
   scale_x_discrete(labels = c("With\nReason", "Parental\nNeglect", "Autonomy\nViolation", 
                               "Marital\nConflict", "Intimacy\nDenial", "Household\nMistake"))+
   labs(
-    title = "Regional Views on Domestic Violence Tolerance",
-    subtitle = "Comparison of public opinion on different causes of domestic violence \n across developing countries in 2020",
+    title = "Public Opinions on Causes of Domestic Violence in Developing Countries",
+    subtitle = "Survey Results from 2020 Highlighting Regional Support \nfor Different Justifications of Domestic Violence",
     x = NULL,
-    y = "Number of People Supporting",
-    fill = "Subregion"
+    y = "Number of People Supporting"
   )+
   theme(
     plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
     plot.subtitle = element_text(face = "italic", hjust = 0.5),
     axis.title = element_text(size = 14),
-    axis.text.x = element_text(size = 12, angle = 45, hjust = 1),
+    axis.text.x = element_text(size = 12, hjust = 1),
     axis.text.y = element_text(size = 12),
-    legend.title = element_text(face = "bold")
   )
 ggsave(filename = "domestic_opinion.png", 
        path = "figure_africa_asia")
